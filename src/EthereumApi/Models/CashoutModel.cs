0644afcfea44e084ed89ff29e5dde05aa0207910
AleksandrBelousov <tortyt1@gmail.com>
2017-05-19 16:09:56 +0400

    Made exchange controller interface easy to understand, simplified exchange models

--- src/EthereumApi/Models/CashoutModel.cs
+++ src/EthereumApi/Models/CashoutModel.cs
@@ -8,20 +8,7 @@ using System.Threading.Tasks;
 namespace EthereumApi.Models
 {
     public class CashoutModel : BaseCoinRequestModel
-    {
-        [Required]
-        public string Coin { get; set; }
-
-        [Required]
-        public string Client { get; set; }
-
-        [Required]
-        public string To { get; set; }
-
-        [Required]
-        [RegularExpression(Constants.BigIntTemplate)]
-        public string Amount { get; set; }
-
+    { 
         [Required]
         public string Sign { get; set; }
     }
