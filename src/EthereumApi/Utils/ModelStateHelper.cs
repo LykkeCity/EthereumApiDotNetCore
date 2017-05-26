0fbd7cc83df57b24ac3cada2728e138fd9c2ae1a
AleksandrBelousov <tortyt1@gmail.com>
2017-05-26 16:37:56 +0400

    updated validation

---
+++ src/EthereumApi/Utils/ModelStateHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EthereumApi.Utils
+{
+    public static class ModelStateHelper
+    {
+        public static IEnumerable Errors(this ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid)
+            {
+                return modelState.ToDictionary(kvp => kvp.Key,
+                    kvp => kvp.Value.Errors
+                                    .Select(e => e.ErrorMessage).ToArray())
+                                    .Where(m => m.Value.Count() > 0);
+            }
+            return null;
+        }
+    }
+}
