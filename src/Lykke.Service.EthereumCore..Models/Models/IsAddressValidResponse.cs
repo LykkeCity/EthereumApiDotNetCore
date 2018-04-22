aee79901b06dad769e2f25596e54ae3b59540e58
AleksandrBelousov <tortyt1@gmail.com>
2018-04-22 18:51:37 +0400

    Added validation controller for eth address

---
+++ src/Lykke.Service.EthereumCore..Models/Models/IsAddressValidResponse.cs
@@ -0,0 +1,17 @@
+﻿using Lykke.Service.EthereumCore.Core;
+using Lykke.Service.EthereumCore.Models.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Lykke.Service.EthereumCore.Models.Models
+{
+    [DataContract]
+    public class IsAddressValidResponse
+    {
+        [DataMember]
+        public bool IsValid { get; set; }
+    }
+}
