3b3245220a4661e8474b8637bac9b1925e97b634
AleksandrBelousov <tortyt1@gmail.com>
2018-05-25 13:46:56 +0400

    Etherscan now shows token transfers

--- src/ContractBuilder/contracts/token/Erc223/lykkeTokenErc223Base.sol
+++ src/ContractBuilder/contracts/token/Erc223/lykkeTokenErc223Base.sol
@@ -6,6 +6,9 @@ contract LykkeTokenErc223Base is ERC223Token {
 
     address internal _issuer;
     string public standard;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
 
     function LykkeTokenErc223Base(
         address issuer,
@@ -13,8 +16,11 @@ contract LykkeTokenErc223Base is ERC223Token {
         uint8 divisibility,
         string tokenSymbol, 
         string version,
-        uint256 totalSupply) ERC223Token(tokenName, tokenSymbol, divisibility, totalSupply) public{
+        uint256 totalSupply) ERC223Token(totalSupply) public{
+        symbol = tokenSymbol;
         standard = version;
+        name = tokenName;
+        decimals = divisibility;
         _issuer = issuer;
     }
 }
