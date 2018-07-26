5995df9d3ebd0fdcffe9ca7137dfc3c9964cd9c9
AleksandrBelousov <tortyt1@gmail.com>
2018-07-27 00:00:02 +0400

    Transfer from coin adapter segment =)

--- src/DepostContractResolver/Program.cs
+++ src/DepostContractResolver/Program.cs
@@ -27,21 +27,54 @@ namespace DepositContractResolver
             application.Name = "DepositContractResolver";
             application.Description = ".NET Core console app to retrieve funds from deposit address and legacy coin adapter.";
 
-            application.Command("cub", lineApplication =>
+            application.Command("transfer-from-deposit", lineApplication =>
             {
+                lineApplication.Description = "This is the description for transfer-from-deposit.";
+                lineApplication.HelpOption("-?|-h|--help");
 
+                var settingsUrlOption = application.Option("-s|--settings <optionvalue>", 
+                    "ethereum core SettingsUrl", 
+                    CommandOptionType.SingleValue);
+                var fromAddressOption =
+                    application.Option("-f|--from-address <optionvalue>", 
+                        "FromAddress is an address of user old(legacy) deposit address", 
+                        CommandOptionType.SingleValue);
+                var toAddressOption =
+                    application.Option("-t|--to-address <optionvalue>",
+                        "ToAddress is the user's address of ETH asset in blockchain integration layer",
+                        CommandOptionType.SingleValue);
+
+                lineApplication.OnExecute(async () =>
+                    await TransferFromLegacyDepositToUsersEthDepositAsync(
+                        settingsUrlOption.Value(),
+                        fromAddressOption.Value(),
+                          toAddressOption.Value()));
             }, true);
-            var settingsUrlArg = application.Argument("SettingsUrl", "ethereum core SettingsUrl", false);
-            var fromAddressArg = 
-                application.Argument("FromAddress", "FromAddress is an address of user old(legacy) deposit address", false);
-            var toAddressArg = 
-                application.Argument("ToAddress", "ToAddress is the user's address of ETH asset in blockchain integration layer", false);
 
-            application.OnExecute(async () => 
-                await TransferFromLegacyDepositToUsersEthDepositAsync(
-                    settingsUrlArg.Value,
-                    fromAddressArg.Value,
-                    toAddressArg.Value));
+            application.Command("transfer-from-adapter", lineApplication =>
+            {
+                lineApplication.Description = "This is the description for transfer-from-adapter.";
+                lineApplication.HelpOption("-?|-h|--help");
+
+                var settingsUrlOption = application.Option("-s|--settings <optionvalue>",
+                    "ethereum core SettingsUrl",
+                    CommandOptionType.SingleValue);
+                var fromAddressOption =
+                    application.Option("-f|--from-address <optionvalue>",
+                        "FromAddress is an address of user old(legacy) deposit address",
+                        CommandOptionType.SingleValue);
+                var toAddressOption =
+                    application.Option("-t|--to-address <optionvalue>",
+                        "ToAddress is the user's address of ETH asset in blockchain integration layer",
+                        CommandOptionType.SingleValue);
+
+                lineApplication.OnExecute(async () =>
+                    await TransferFromLegacyDepositToUsersEthDepositAsync(
+                        settingsUrlOption.Value(),
+                        fromAddressOption.Value(),
+                        toAddressOption.Value()));
+            }, true);
+
             application.Execute(args);
         }
 
@@ -53,29 +86,7 @@ namespace DepositContractResolver
             #region RegisterDependencies
 
             var appSettings = GetCurrentSettingsFromUrl(settingsUrl);
-            ContainerBuilder containerBuilder = new ContainerBuilder();
-            IServiceCollection collection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
-            containerBuilder.RegisterInstance(appSettings);
-            containerBuilder.RegisterInstance<IBaseSettings>(appSettings.CurrentValue.EthereumCore);
-            containerBuilder.RegisterInstance<ISlackNotificationSettings>(appSettings.CurrentValue.SlackNotifications);
-            containerBuilder.RegisterInstance(appSettings.Nested(x => x.EthereumCore));
-            containerBuilder.RegisterInstance(appSettings.CurrentValue);
-            var consoleLogger = new LogToConsole();
-            collection.AddSingleton<ILog>(consoleLogger);
-            RegisterReposExt.RegisterAzureQueues(containerBuilder, appSettings.Nested(x => x.EthereumCore.Db.DataConnString),
-                appSettings.Nested(x => x.SlackNotifications));
-            RegisterReposExt.RegisterAzureStorages(containerBuilder, appSettings.Nested(x => x.EthereumCore),
-                appSettings.Nested(x => x.SlackNotifications), consoleLogger);
-            RegisterRabbitQueueEx.RegisterRabbitQueue(collection,
-                appSettings.Nested(x => x.EthereumCore.RabbitMq),
-                appSettings.Nested(x => x.EthereumCore.Db.DataConnString),
-                consoleLogger);
-            RegisterDependency.RegisterServices(collection);
-            RegisterDependency.RegisterServices(containerBuilder);
-            containerBuilder.Populate(collection);
-            containerBuilder.RegisterInstance<ILog>(consoleLogger);
-            var resolver = containerBuilder.Build();
-            resolver.ActivateRequestInterceptor();
+            var (resolver, consoleLogger) = GetResolver(appSettings);
 
             #endregion
 
@@ -105,7 +116,7 @@ namespace DepositContractResolver
             }
 
             var transactionHash = await transferContractService.RecievePaymentFromTransferContract(
-                oldDeposiTransferContract.ContractAddress, 
+                oldDeposiTransferContract.ContractAddress,
                 oldDeposiTransferContract.CoinAdapterAddress);
 
             await consoleLogger.WriteInfoAsync(nameof(TransferFromLegacyDepositToUsersEthDepositAsync),
@@ -122,7 +133,7 @@ namespace DepositContractResolver
             string transactionHashFromAdapter = await exchangeContractService.CashOutWithoutSignCheck(guid,
                 oldDeposiTransferContract.CoinAdapterAddress,
                 oldDeposiTransferContract.UserAddress,
-                toAddress, 
+                toAddress,
                 balance);
 
             await consoleLogger.WriteInfoAsync(nameof(TransferFromLegacyDepositToUsersEthDepositAsync),
@@ -135,6 +146,87 @@ namespace DepositContractResolver
                                         $"https://etherscan.io/tx/{transactionHashFromAdapter}");
 
             return 0;
+        }
+
+        static async Task<int> TransferFromAdapterToUsersEthDepositAsync(
+            string settingsUrl,
+            string coinAdapter,
+            string fromAddress,
+            string toAddress)
+        {
+            #region RegisterDependencies
+
+            var appSettings = GetCurrentSettingsFromUrl(settingsUrl);
+            var (resolver, consoleLogger) = GetResolver(appSettings);
+
+            #endregion
+
+            string depositContractAddress = fromAddress?.ToLower();
+            var transferContractRepository = resolver.Resolve<ITransferContractRepository>();
+            var transferContractService = resolver.Resolve<ITransferContractService>();
+            var contractService = resolver.Resolve<IContractService>();
+            var exchangeContractService = resolver.Resolve<IExchangeContractService>();
+            var oldDeposiTransferContract = await transferContractRepository.GetAsync(depositContractAddress);
+
+            BigInteger adapterBalance = await transferContractService.GetBalanceOnAdapter(
+                oldDeposiTransferContract.CoinAdapterAddress,
+                oldDeposiTransferContract.UserAddress,
+                checkInPendingBlock: true);
+
+            if (adapterBalance == 0)
+            {
+                await consoleLogger.WriteInfoAsync(nameof(TransferFromAdapterToUsersEthDepositAsync),
+                    depositContractAddress, "Adapter balance is 0");
+
+                return 0;
+            }
+
+            var guid = Guid.NewGuid();
+            string transactionHashFromAdapter = await exchangeContractService.CashOutWithoutSignCheck(guid,
+                coinAdapter,
+                fromAddress,
+                toAddress,
+                adapterBalance);
+
+            await consoleLogger.WriteInfoAsync(nameof(TransferFromAdapterToUsersEthDepositAsync),
+                depositContractAddress, $"Transfer from the adapter address to the destination is pending. {transactionHashFromAdapter}");
+
+            await contractService.WaitForTransactionToCompleteAsync(transactionHashFromAdapter);
+
+            await consoleLogger.WriteInfoAsync(nameof(TransferFromAdapterToUsersEthDepositAsync),
+                depositContractAddress, $"Transfer to the destination address is completed. check:" +
+                                        $"https://etherscan.io/tx/{transactionHashFromAdapter}");
+
+            return 0;
+        }
+
+        private static (IContainer resolver, ILog logToConsole) GetResolver(IReloadingManager<AppSettings> appSettings)
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            IServiceCollection collection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            containerBuilder.RegisterInstance(appSettings);
+            containerBuilder.RegisterInstance<IBaseSettings>(appSettings.CurrentValue.EthereumCore);
+            containerBuilder.RegisterInstance<ISlackNotificationSettings>(appSettings.CurrentValue.SlackNotifications);
+            containerBuilder.RegisterInstance(appSettings.Nested(x => x.EthereumCore));
+            containerBuilder.RegisterInstance(appSettings.CurrentValue);
+            var consoleLogger = new LogToConsole();
+            collection.AddSingleton<ILog>(consoleLogger);
+            RegisterReposExt.RegisterAzureQueues(containerBuilder, appSettings.Nested(x => x.EthereumCore.Db.DataConnString),
+                appSettings.Nested(x => x.SlackNotifications));
+            RegisterReposExt.RegisterAzureStorages(containerBuilder, appSettings.Nested(x => x.EthereumCore),
+                appSettings.Nested(x => x.SlackNotifications), consoleLogger);
+            RegisterRabbitQueueEx.RegisterRabbitQueue(collection,
+                appSettings.Nested(x => x.EthereumCore.RabbitMq),
+                appSettings.Nested(x => x.EthereumCore.Db.DataConnString),
+                consoleLogger);
+            RegisterDependency.RegisterServices(collection);
+            RegisterDependency.RegisterServices(containerBuilder);
+            containerBuilder.Populate(collection);
+            containerBuilder.RegisterInstance<ILog>(consoleLogger);
+            var resolver = containerBuilder.Build();
+            resolver.ActivateRequestInterceptor();
+
+            return (resolver, consoleLogger);
         }
 
         static IReloadingManager<AppSettings> GetCurrentSettingsFromUrl(string settingsUrl)
