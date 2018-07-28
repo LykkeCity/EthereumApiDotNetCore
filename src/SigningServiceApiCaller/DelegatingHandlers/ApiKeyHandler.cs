c3616f96fa3530ced63d6d6b2f0eb10ea577669f
AleksandrBelousov <tortyt1@gmail.com>
2018-07-28 14:56:21 +0400

    rebased on candidate

---
+++ src/SigningServiceApiCaller/DelegatingHandlers/ApiKeyHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SigningServiceApiCaller.DelegatingHandlers
+{
+    public class ApiKeyHandler : DelegatingHandler
+    {
+        private readonly string _apiKeyValue;
+
+        public ApiKeyHandler(string apiKeyValue)
+        {
+            _apiKeyValue = apiKeyValue;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            request.Headers.Add("apiKey", _apiKeyValue);
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            return response;
+        }
+    }
+}
