0b043bb1e1b3e91d01b764338d0f9c4bc764632a
AleksandrBelousov <tortyt1@gmail.com>
2018-09-06 15:53:45 +0400

    Added console to extract erc223 deposit addresses

---
+++ src/PassTokenAddressExporter/Commands/CommandFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PassTokenAddressExporter.Helpers;
+
+namespace PassTokenAddressExporter.Commands
+{
+    public class CommandFactory
+    {
+        private readonly IConfigurationHelper _helper;
+
+        public CommandFactory(IConfigurationHelper helper)
+        {
+            _helper = helper;
+        }
+
+        public ICommand CreateCommand(Func<IConfigurationHelper, ICommand> createFunc)
+        {
+            var command = createFunc(_helper);
+
+            return command;
+        }
+    }
+}
