bf92039d46fb7e202c17dca3566984c7119bea94
Aleksandr Belousov <alexandr.belousov@lykke.com>
2019-04-05 10:12:53 +0400

    fixed autorest client

--- src/EthereumSamuraiApiCaller/EthereumSamuraiApi.cs
+++ src/EthereumSamuraiApiCaller/EthereumSamuraiApi.cs
@@ -209,18 +209,18 @@ namespace EthereumSamuraiApiCaller
             // Construct URL
             var _baseUrl = BaseUri.AbsoluteUri;
             var _url = new System.Uri(new System.Uri(_baseUrl + (_baseUrl.EndsWith("/") ? "" : "/")), "api/AddressHistory/{address}").ToString();
-            _url = _url.Replace("{Address}", System.Uri.EscapeDataString(address));
+            _url = _url.Replace("{address}", System.Uri.EscapeDataString(address));
             List<string> _queryParameters = new List<string>();
             if (startBlock != null)
             {
-                _queryParameters.Add(string.Format("StartBlock={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(startBlock, SerializationSettings).Trim('"'))));
+                _queryParameters.Add(string.Format("startBlock={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(startBlock, SerializationSettings).Trim('"'))));
             }
             if (stopBlock != null)
             {
-                _queryParameters.Add(string.Format("StopBlock={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(stopBlock, SerializationSettings).Trim('"'))));
+                _queryParameters.Add(string.Format("stopBlock={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(stopBlock, SerializationSettings).Trim('"'))));
             }
-            _queryParameters.Add(string.Format("Count={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(count, SerializationSettings).Trim('"'))));
-            _queryParameters.Add(string.Format("Start={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(start, SerializationSettings).Trim('"'))));
+            _queryParameters.Add(string.Format("count={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(count, SerializationSettings).Trim('"'))));
+            _queryParameters.Add(string.Format("start={0}", System.Uri.EscapeDataString(SafeJsonConvert.SerializeObject(start, SerializationSettings).Trim('"'))));
             if (_queryParameters.Count > 0)
             {
                 _url += "?" + string.Join("&", _queryParameters);
